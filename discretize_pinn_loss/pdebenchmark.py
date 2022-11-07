c3071746d7151099be90820f37385fc9f68656fd
Adrien B <forbu14@gmail.com>
2022-11-07 22:49:12 +0100

    adding classic dataset to compare with FNO1d

--- discretize_pinn_loss/pdebenchmark.py
+++ discretize_pinn_loss/pdebenchmark.py
@@ -5,13 +5,17 @@ Module where we use the PDE dataset to create our dataloader
 
 # import pytorch dataset and dataloader
 import torch
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 from torch_geometric.data import Data
 from torch_geometric.data import Dataset as GeometricDataset
 import h5py
 import numpy as np
 
 class BurgerPDEDataset(GeometricDataset):
+    """
+    This is a PDE dataset that we use to create our dataloader
+    This is in graph mode using the torch geometric library
+    """
     def __init__(self, path_hdf5, edges, edges_index, mask=None):
         super(BurgerPDEDataset, self).__init__()
 
@@ -63,6 +67,59 @@ class BurgerPDEDataset(GeometricDataset):
                     edge_index=torch.tensor(self.edges_index).T.long(), mask=torch.tensor(self.mask).float(), x_next=torch.tensor(tensor_tp1).float())
 
         return graph
+
+
+class BurgerPDEDataset_clasic(Dataset):
+    """
+    This is a PDE dataset that we use to create our dataloader
+    This is in graph mode using the torch geometric library
+    """
+    def __init__(self, path_hdf5, mask=None):
+        super(BurgerPDEDataset, self).__init__()
+
+        self.path_hdf5 = path_hdf5
+        self.mask = mask
+
+        # first read to know the lenght of the dataset
+        with h5py.File(self.path_hdf5, "r") as f:
+            
+            # we get the shape of f['tensor']
+            tensor_arr = f['tensor']
+
+            self.len_item = tensor_arr.shape[0]
+            
+            self.t = np.array(f['t-coordinate'])
+            self.len_t = self.t.shape[0]
+
+            self.x = np.array(f['x-coordinate'])
+
+        if self.mask is None:
+            self.mask = np.ones(self.x.shape)
+            
+    def __len__(self):
+        return self.len_item * (self.len_t - 3)
+
+    def __getitem__(self, idx):
+
+        # we get the index of the time step
+        idx_t = idx // (self.len_item - 3)
+
+        # we get the index of the item
+        idx_item = idx % self.len_item
+        
+        # we open the hdf5 file
+        with h5py.File(self.path_hdf5, "r") as f:
+            # we get the tensor
+            tensor_arr = f['tensor']
+            # we get the tensor at the index idx
+            tensor = np.array(tensor_arr[idx_item, idx_t, :])
+
+            # we concat the tensor with the mask
+            tensor = np.concatenate([tensor.reshape((-1, 1)), self.mask], axis=1)
+
+            tensor_tp1 = np.array(tensor_arr[idx_item, idx_t+1, :])
+
+        return torch.tensor(tensor, dtype=torch.float32), torch.tensor(tensor_tp1, dtype=torch.float32)
 
 class BurgerPDEDatasetFullSimulation(Dataset):
     def __init__(self, path_hdf5, edges, edges_index, mask=None):
