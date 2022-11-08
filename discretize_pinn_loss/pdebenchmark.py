cb54c5faa7843597741d2a9af7178cc9d74d02f6
Adrien B <forbu14@gmail.com>
2022-11-08 22:03:27 +0100

    adding temporal dataset to test stability

--- discretize_pinn_loss/pdebenchmark.py
+++ discretize_pinn_loss/pdebenchmark.py
@@ -121,6 +121,64 @@ class BurgerPDEDataset_classic(Dataset):
 
         return torch.tensor(tensor, dtype=torch.float32), torch.tensor(tensor_tp1, dtype=torch.float32), torch.tensor(self.mask)
 
+
+
+class BurgerPDEDatasetMultiTemporal(Dataset):
+    """
+    This is a PDE dataset that we use to create our dataloader
+    This is in graph mode using the torch geometric library
+    """
+    def __init__(self, path_hdf5, mask=None, timesteps=3):
+        super(BurgerPDEDataset_classic, self).__init__()
+
+        self.path_hdf5 = path_hdf5
+        self.mask = mask
+        self.timesteps = timesteps
+
+        # first read to know the lenght of the dataset
+        with h5py.File(self.path_hdf5, "r") as f:
+            
+            # we get the shape of f['tensor']
+            tensor_arr = f['tensor']
+
+            self.len_item = tensor_arr.shape[0]
+            
+            self.t = np.array(f['t-coordinate'])
+            self.len_t = self.t.shape[0]
+
+            self.x = np.array(f['x-coordinate'])
+
+        if self.mask is None:
+            self.mask = np.ones(self.x.shape)
+            
+    def __len__(self):
+        return self.len_item * (self.len_t - 3 - self.timesteps)
+
+    def __getitem__(self, idx):
+
+        # we get the index of the time step
+        idx_t = idx // (self.len_item - 3 - self.timesteps)
+
+        # we get the index of the item
+        idx_item = idx % self.len_item
+        
+        # we open the hdf5 file
+        with h5py.File(self.path_hdf5, "r") as f:
+            # we get the tensor
+            tensor_arr = f['tensor']
+            # we get the tensor at the index idx
+            tensor = np.array(tensor_arr[idx_item, idx_t, :])
+
+            # we concat the tensor with the mask
+            tensor = tensor.reshape((-1, 1))
+
+            tensor_to_predict = np.array(tensor_arr[idx_item, idx_t:(idx_t+self.timesteps), :]).reshape((-1, self.timesteps))
+
+        return torch.tensor(tensor, dtype=torch.float32), torch.tensor(tensor_to_predict), torch.tensor(self.mask)
+
+
+
+
 class BurgerPDEDatasetFullSimulation(Dataset):
     def __init__(self, path_hdf5, edges, edges_index, mask=None):
         self.path_hdf5 = path_hdf5
