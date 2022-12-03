da681dd6a131588c43f6bd7c4ff4d82f07a71c5f
Adrien B <forbu14@gmail.com>
2022-12-03 11:47:40 +0100

    Create losses_CFD.py

---
+++ discretize_pinn_loss/losses_CFD.py
@@ -0,0 +1,9 @@
+from torch.nn import Module        
+from torch import nn, cat
+from torch_geometric.nn import MetaLayer
+from torch_scatter import scatter_sum, scatter_mean
+from torch_geometric.data import Data
+import torch
+
+import math
+from torch_geometric.nn import MetaLayer
